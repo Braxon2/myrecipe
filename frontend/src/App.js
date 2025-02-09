866532ca1e63e47e2d29ae7d3da82f87c6c73f93
Braxon2 <dusanb80@gmail.com>
2025-02-10 00:19:18 +0100

    Added changes what nav elements can you see and navigation when login in and signing in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import RecipeForm from "./components/RecipeForm";
@@ -7,18 +9,32 @@ import Signup from "./components/Signup";
 import LoginPage from "./components/LoginPage";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipes" element={<Home />} />
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/recipes"
+              element={user ? <Home /> : <Navigate to="/login" />}
+            />
             <Route path="/create" element={<RecipeForm />} />
             <Route path="/recipes/:id" element={<RecipeDetails />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <LoginPage /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
